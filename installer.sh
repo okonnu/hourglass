ca5bb284c4e766b06fbe829d41b552570d6f1267
okonu <d.okonu1@gmail.com>
2021-11-18 18:18:51 -0600

    auto - cases sum

--- installer.sh
+++ installer.sh
@@ -15,12 +15,12 @@ printf '%s\n' '[Desktop Entry]' 'Type=Application' 'Name=startup' 'Exec=/home/pi
 echo 'you can now add commands you want to run on boot on the startup file. they will run after the GUI has loaded' &&
 
 echo 'creating cronjobs to restart the raspberry every 6am, 2am and 10pm. always ensure that there is internet so that the system time will always be accurate' &&
-let cronjob1="0 6 * * * /sbin/shutdown -r now"
-let cronjob2="0 14 * * * /sbin/shutdown -r now"
-let cronjob3="0 22 * * * /sbin/shutdown -r now"
-( crontab -l | grep -v -F "$cronjob1" ; echo "$cronjob1" ) | crontab -
-( crontab -l | grep -v -F "$cronjob2" ; echo "$cronjob2" ) | crontab -
-( crontab -l | grep -v -F "$cronjob3" ; echo "$cronjob3" ) | crontab -
+let cronjob1="0 6 * * * /sbin/shutdown -r now" &&
+let cronjob2="0 14 * * * /sbin/shutdown -r now" &&
+let cronjob3="0 22 * * * /sbin/shutdown -r now" &&
+( crontab -l | grep -v -F "$cronjob1" ; echo "$cronjob1" ) | crontab - &&
+( crontab -l | grep -v -F "$cronjob2" ; echo "$cronjob2" ) | crontab - &&
+( crontab -l | grep -v -F "$cronjob3" ; echo "$cronjob3" ) | crontab - &&
 
 echo 'installation complete :) ...'
 
