625ea33e179d89ac1a751eb25d939c22eb99ce78
okonu <d.okonu1@gmail.com>
2021-11-20 00:58:47 -0600

    auto - cases sum

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
 
 const left_l = 'L6'
-const right_l = 'L3'
+const right_l = 'L5'
 let queue1 = []
 let queue2 = []
 const target_l = 300
