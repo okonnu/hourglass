e3b886e4f58495d4acf3cc1a296af2a2032b5ef0
okonu <d.okonu1@gmail.com>
2021-12-02 03:54:38 -0600

    added kill switch

--- gethourly.py
+++ gethourly.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 def hcases(line):
     url = os.getenv('GET_HCASES_URL') + line
-    # x = requests.get(url)
+    x = requests.get(url)
     print('getting hourly for line'+ line)
     print('---------------')
-    # print(x.text)
+    print(x.text)
     print('---------------')
     # return (x.text)
 
-    return '{"data": [{"hr": 6,"line": "L6","cans": 4551,"cases": 189,"config_id": 2208}, {"hr": 6,"line": "L6","cans": 4551,"cases": 189,"config_id": 2208}, {"hr": 7,"line": "L6","cans": 6020,"cases": 250,"config_id": 2208}]}'
+    # return '{"data": [{"hr": 6,"line": "L6","cans": 4551,"cases": 189,"config_id": 2208}, {"hr": 6,"line": "L6","cans": 4551,"cases": 189,"config_id": 2208}, {"hr": 7,"line": "L6","cans": 6020,"cases": 250,"config_id": 2208}]}'
