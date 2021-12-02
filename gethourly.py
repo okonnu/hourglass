f1a9fb12487c0c89bf7a4f7240fbc4325d3a8a98
okonu <d.okonu1@gmail.com>
2021-12-02 04:59:35 -0600

    fgp aded

--- gethourly.py
+++ gethourly.py
@@ -9,6 +9,6 @@ def hcases(line):
     print('---------------')
     print(x.text)
     print('---------------')
-    # return (x.text)
+    return (x.text)
 
     # return '{"data": [{"hr": 6,"line": "L6","cans": 4551,"cases": 189,"config_id": 2208}, {"hr": 6,"line": "L6","cans": 4551,"cases": 189,"config_id": 2208}, {"hr": 7,"line": "L6","cans": 6020,"cases": 250,"config_id": 2208}]}'
