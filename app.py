fe3f1bc95ab565213036a2e1781421443d74e385
okonu <d.okonu1@gmail.com>
2021-12-24 18:02:36 -0600

    auto commit

--- app.py
+++ app.py
@@ -7,13 +7,9 @@ from gethourly import hcases
 import os
 from dotenv import load_dotenv
 from resetRpi import restart
-import subprocess
+from cleanchrome import cleanchrome
 
-bashCommand = 'sed -i \'s/"exited_cleanly":false/"exited_cleanly":true/\' ~/.config/chromium/Default/Preferences;sed -i \'s/"exit_type": "Crashed"/"exit_type": "Normal"/\' ~/.config/chromium/Default/Preferences'
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
-
-
+cleanchrome()
 
 eel.init('web')
 load_dotenv()
---
+++ cleanchrome.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def cleanchrome():
+    bashCommand = 'sed -i \'s/"exited_cleanly":false/"exited_cleanly":true/\' ~/.config/chromium/Default/Preferences;sed -i \'s/"exit_type": "Crashed"/"exit_type": "Normal"/\' ~/.config/chromium/Default/Preferences'
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    
+    
