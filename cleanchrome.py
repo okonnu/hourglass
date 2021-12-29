818ab39839c76efda0346b165330c70b024b33de
okonu <d.okonu1@gmail.com>
2021-12-28 19:54:44 -0600

    added code to clean chrome

--- cleanchrome.py
+++ cleanchrome.py
@@ -1,8 +1,6 @@
+import os
 import subprocess
 
 def cleanchrome():
-    bashCommand = 'sed -i \'s/"exited_cleanly":false/"exited_cleanly":true/\' ~/.config/chromium/Default/Preferences;sed -i \'s/"exit_type": "Crashed"/"exit_type": "Normal"/\' ~/.config/chromium/Default/Preferences'
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    
-cleanchrome()
+    os.chmod("cleanchrome.sh", 509)
+    rc = subprocess.call("./cleanchrome.sh")
