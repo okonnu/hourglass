d3847f1507f94f08916b6cb70187b4214fc4c8bc
okonu <d.okonu1@gmail.com>
2021-12-30 15:58:51 -0600

    chr

--- cleanchrome.sh
+++ cleanchrome.sh
@@ -1,4 +1,4 @@
 #!/bin/sh   
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
-sed -i 's/"exit_type": "Crashed"/"exit_type": "Normal"/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type": "Crashed"/"exit_type": "Normal"/' /home/pi/.config/chromium/Default/Preferences
 echo "cleaned chromium error reports"
