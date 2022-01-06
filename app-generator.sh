9750de451447ee9d7fae3db5d1f34c211823078a
okonu <d.okonu1@gmail.com>
2022-01-06 00:10:00 -0600

    blah

--- app-generator.sh
+++ app-generator.sh
@@ -2,4 +2,4 @@ pip3 install eel
 pip3 install PyInstaller
 pip3 install gtts
 pip3 install playsound
-python -m eel app.py web --onefile --noconsole --icon=logo.ico
+python3 -m eel app.py web --onefile --noconsole --icon=logo.ico
---
+++ win.py
@@ -0,0 +1,13 @@
+import eel
+from gtts import gTTS
+from playsound import playsound
+
+eel.init('web')
+@eel.expose
+def txttospeech(txt,indx):
+    tts = gTTS(text=txt, lang='en', slow=False)
+    tts.save(str(indx)+"voice.mp3")
+    playsound(str(indx)+"voice.mp3")
+
+# txttospeech('three minutes remaining')
+eel.start('index.html', mode='edge', host='localhost', port=27011, size=(250,150), position=(0,0), cmdline_args=['--disable-infobars','--Kiosk'] )
