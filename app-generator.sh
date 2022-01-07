6cd3178962a4ec7ee90f61e783c41c9eff1c972d
unknown <d.okonu1@gmail.com>
2022-01-07 01:24:43 -0600

    rf

--- app-generator.sh
+++ app-generator.sh
@@ -1,5 +1,6 @@
 pip3 install eel
 pip3 install PyInstaller
 pip3 install gtts
-pip3 install playsound
+pip uninstall playsound
+pip install playsound==1.2.2
 python3 -m eel hourglass.py web --onefile --noconsole --icon=logo.ico
